8aa3d8f021fc0feb1d08fb8ab3d895757b15f4ea
taksite <91469283+taksite@users.noreply.github.com>
2021-12-11 12:51:24 +0100

    add coments in templateToHtml function

--- prg/templateToHtml.php
+++ prg/templateToHtml.php
@@ -6,7 +6,10 @@ declare(strict_types=1);
 
 # zamienia wczytany szablon na html + odczyt zmiennych do wstawienia z tabeli 
 # index tabeli to nazwa wystawiana w pliku pod postacią: {$var$}, gdzie var to index tabeli
-
+# ========================================================================================================
+# converts the loaded template to html + read variables to be inserted from the table
+# table index is the name displayed in the file in the form: {$ var $}, where var is the table index
+# ========================================================================================================
 
 function templateToHtml (string $template, array $table) : string
 
@@ -23,6 +26,6 @@ function templateToHtml (string $template, array $table) : string
         $templt=str_replace("{\$".$ind."\$}", $data, $templt);
 
     }
-    $templt=preg_replace('({\$(.*?)\$})', "zapomniales!", $templt);   //wstawianie do pozostałości nie uwzględnionych w bazie
+    $templt=preg_replace('({\$(.*?)\$})', "zapomniales!", $templt);   //inserting if not included in the template
     return $templt;
 }
